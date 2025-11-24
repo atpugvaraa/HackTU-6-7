001dd965a588b893157fbbe30f2d3b287984fc2d
HARSHJYOT_KAUR <68178422+MAHI21-WEB@users.noreply.github.com>
2025-11-24 10:03:59 +0530

    Timeline TBA

--- src/Pages/Timeline/timelineElements.ts
+++ src/Pages/Timeline/timelineElements.ts
@@ -1,16 +1,6 @@
 export const timelineElements = [
 	{
 		key: 1,
-		time: "28 November",
-		icon: "work",
-		title: "Registrations Open",
-		buttonText: "Read More",
-		//   location: "",
-		description:
-			"Get ready to innovate! The registrations for HackTU 7.O shall open on 28th November 2025. Don't miss your chance to be part of this exciting event!",
-	},
-	{
-		key: 2,
 		time: "To be Announced",
 		icon: "school",
 		title: "To be Announced",
